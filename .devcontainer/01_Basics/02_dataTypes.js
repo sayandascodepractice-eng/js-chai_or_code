5726fc40f8a7f030babb7154f490351cb91456cc
sayandascodepractice-eng <sayandascodepractice@gmail.com>
2025-08-10 03:34:53 +0000

    Push

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browswer
+
+let name = "Sayan"
+let age = 18
+let isLoggedIn
+let state;
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+// object
